ea4af24b5eb1b439fab38b8804ae5ae230011993
Kpriyad2909 <kpriyad2909@gmail.com>
2025-09-16 11:33:45 +0530

    Resolved all merge conflicts

--- tasks/models.py
+++ tasks/models.py
@@ -6,11 +6,7 @@ class Tasks(models.Model):
     duedate = models.DateField()
     priority = models.CharField(null=False,blank=False, choices=[('high', 'high'),('medium', 'medium'),('low', 'low')])  
     status = models.CharField(max_length=20)   
-<<<<<<< HEAD
     Task_status = models.CharField(null=False,blank=False,max_length=20, choices=[("Pending", "Pending"), ("In Progress", "In Progress"),("Completed", "Completed")]) 
-=======
-    completed = models.CharField(null=False,blank=False,max_length=20, choices=[("Pending", "Pending"), ("In Progress", "In Progress"),("Completed", "Completed")]) 
->>>>>>> 243e48f1faac87284f54b9eda3acb04c14e4f464
     
 
     def __str__(self):
